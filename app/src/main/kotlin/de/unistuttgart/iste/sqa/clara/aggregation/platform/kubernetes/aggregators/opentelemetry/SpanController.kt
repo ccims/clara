43f53b530c75ed0919777b35416fa8c6435b567b
Elias Müller <elias_benedikt.mueller@porsche.de>
2023-12-14 12:26:29 +0100

    feat: WIP: some data objects and further ideas for recreation

--- app/src/main/kotlin/de/unistuttgart/iste/sqa/clara/aggregation/platform/kubernetes/aggregators/opentelemetry/SpanController.kt
+++ app/src/main/kotlin/de/unistuttgart/iste/sqa/clara/aggregation/platform/kubernetes/aggregators/opentelemetry/SpanController.kt
@@ -1,6 +1,9 @@
 package de.unistuttgart.iste.sqa.clara.aggregation.platform.kubernetes.aggregators.opentelemetry
 
 
+import de.unistuttgart.iste.sqa.clara.aggregation.platform.kubernetes.aggregators.opentelemetry.module.Span
+import de.unistuttgart.iste.sqa.clara.aggregation.platform.kubernetes.aggregators.opentelemetry.module.SpanInformation
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.opentelemetry.api.GlobalOpenTelemetry
 import java.math.BigInteger
 import java.net.InetAddress
@@ -9,18 +12,23 @@ import java.util.concurrent.Executor
 import java.util.function.Function
 import java.util.regex.Pattern
 import javax.sql.DataSource
-import io.opentelemetry.api.trace.Span
 import io.opentelemetry.api.trace.Tracer
-
+import java.net.URL
 
 
 // TODO 95% of the code is copied from https://github.com/ga52can/microlyze and has not properly been modified yet
+// Activities are entire business activities meaning the whole span of a trace
+// Services are actual Microservices
+// Instances are instances of microservices
+// Hardware is the used hardware (don't know if necessary)
 
 // TODO the Span Class from open telemetry is more useful for actually generating spans and exporting it and not for working with it
 // TODO therefore we should add a custom Implementation that inherits from it and contains all the attributes as properties, we need
 // TODO we have to read this then from some sort of data source, where we dumped the spans beforehand
-/*
-class SpanController () {
+
+class SpanController() {
+
+    private val log = KotlinLogging.logger {}
 
     /*private fun initialize() {
         componentRevisionMap = revisionService.getCurrentRevisionsByComponentName()
@@ -28,8 +36,7 @@ class SpanController () {
 
     // Proceeding of all ingoing spans via a stream or the ZipKin-REST API.
     // components and relations of them are discovered and persistently stored here
-    @Synchronized
-    // TODO where to get the spannamese from
+    @Synchronized // TODO where to get the spannamese from
     fun proceedSpans(spans: List<String>) {
         for (spanName in spans) {
 
@@ -37,7 +44,7 @@ class SpanController () {
 
             // FIXME get the spanname from somewhere
             // There probably ust be some method somewhere
-            val span: Span = tracer.spanBuilder(spanName).startSpan()
+            val span: Span = Span("", "", "", "", "", "", mapOf())
 
             //val tempSpan: Span = tempStorageSpans[span.id]
 
@@ -54,34 +61,106 @@ class SpanController () {
             //// 1  Create Components
             // 1.1  Generate Services, Instances and Hardware components and relations between them from the binary endpoints
 
-            // how does updateComponents look like?
-            // If client:
-            //      server.address -> Map to server component (probably primary key)
-            //      client identifier? -> there might be an instance name based on the instrumentation
-            //      http.url / url.full -> describes the server
-            //      relation caller / callee -> if caller is possible to be determined
-            // If server:
-            //      look for some server identifier e.g. entity.name
-            //      look for some client identifier e.g. user-agent
-            updateComponents()
+            //updateComponents()
 
 
             //// 2  Discover relations between services
             // 2.1  compute the first span of a transaction (usually Zuul's SR-Response to the not instrumented client)
             //      Mapping of the first span's path (name) with a service via the ComponentMapping-collection
             //      and creation of newly discovered relations between activities and services.
-            if (span.parentId == null) {
-                // Get parentId from span -> if there is none you know it's parent
+            if (span.parentId == null) { // Get parentId from span -> if there is none you know it's parent
                 // Get path and method (should be availalbe)
                 val path: String = span.name
                 var method = "GET"
                 for (attribute in span.attributes) {
-                    if (attribute.equalsIgnoreCase("http.method")) {
-                        method = String(attribute.value).uppercase(Locale.getDefault())
+                    if (attribute.key.lowercase() == "http.method") {
+                        method = attribute.value.uppercase()
                     }
                 }
+            }
+        }
+    }
 
+    // how does updateComponents look like?
+    // If client:
+    //      server.address -> Map to server component (probably primary key)
+    //      client identifier? -> there might be an instance name based on the instrumentation
+    //      http.url / url.full -> describes the server
+    //      relation caller / callee -> if caller is possible to be determined
+    // If server:
+    //      look for some server identifier e.g. entity.name
+    //      look for some client identifier e.g. user-agent
+    private fun findBasicClientServerInfo(span: Span): SpanInformation = when (span.spanKind) {
+        "CLIENT" -> {
+            val serverUrl = getServerUrl(span)
+            SpanInformation(clientIdentifier = span.serviceName, serverIdentifier = serverUrl.host, endpoint = serverUrl.path)
+        }
 
+        "SERVER" -> {
+            val serverUrl = getServerUrl(span)
+            SpanInformation(clientIdentifier = null, serverIdentifier = span.serviceName,  endpoint = serverUrl.path )
+        }
+
+        "CONSUMER" -> {
+            log.warn { "Consumer span identified" }
+            throw UnsupportedOperationException()
+        }
+
+        "PRODUCER" -> {
+            log.warn { "Producer span identified" }
+            throw UnsupportedOperationException()
+        }
+
+        else -> {
+            throw UnsupportedOperationException()
+        }
+    }
+
+    private fun getServerUrl(span: Span): URL {
+        val paths = span.attributes.filter { // TODO check if there might be more keys that represent urls
+            it.key.lowercase() == "http.uri" || it.key.lowercase() == "http.url"
+        }.values
+        if (paths.isEmpty()) {
+            log.error { "${span.spanKind} span without http url detected, the attributes are ${span.attributes}" }
+        }
+        return URL(paths.first())
+    }
+/*
+
+    // extracts information about components (Hardware, Service, Instance) from an endpoint-object
+    // and creates new component-objects for them if they are not existing yet.
+    // Additionally it creates relations between these components
+    private fun updateComponents(): Boolean {
+        val serviceName: String = endpoint.serviceName.toUpperCase()
+        val hardwareName = getHardwareName(endpoint)
+        val instanceName = getInstanceName(endpoint)
+        if (instanceName == "unknown") return false
+        if (!componentRevisionMap!!.containsKey(serviceName)) {
+            val service: Service = serviceService.findByName(serviceName)
+            if (service == null) {
+                serviceService.createService(serviceName)
+            } else revisionService.createRevision(service)
+        }
+        if (!componentRevisionMap!!.containsKey(hardwareName)) {
+            val hardware: Hardware = hardwareService.findByName(hardwareName)
+            if (hardware == null) hardwareService.createHardware(hardwareName) else revisionService.createRevision(hardware)
+        }
+        if (!componentRevisionMap!!.containsKey(instanceName)) {
+            var instance: Instance = instanceService.findByName(instanceName)
+            if (instance == null) {
+                instance = instanceService.createInstance(instanceName)
+                instance.setAnnotation("ad.port", endpoint.port.toString())
+                instance.setAnnotation("ad.ip", getHardwareName(endpoint))
+                instanceService.saveInstance(instance)
+            } else revisionService.createRevision(instance)
+            val instanceRevision: Revision? = componentRevisionMap!![instanceName]
+            val serviceRevision: Revision? = componentRevisionMap!![serviceName]
+            val hardwareRevision: Revision? = componentRevisionMap!![hardwareName]
+            relationService.setInstanceRevisionRelations(instanceRevision, serviceRevision, hardwareRevision)
+        }
+        return true
+    }*/
+}/*
                 // 2.1.1 Create discovered relations between service <-> activities
                 var mappingFound = false
                 val deprecatedMappings: MutableList<ComponentMapping> = ArrayList<ComponentMapping>()
@@ -230,7 +309,7 @@ class SpanController () {
         }
     }
 
-    private fun proceedLocalComponentForRelation(spanId: Long?, relation: Relation) {
+    private fun proceedLocalComponentForRelation(spanId: String?, relation: Relation) {
         if (spanId != null) {
             val span: Span? = localComponentSpans[spanId]
             if (span != null) {
@@ -329,40 +408,6 @@ class SpanController () {
         val hardwareName = getHardwareName(endpoint)
         if (!revisionService.getCurrentRevisionsByComponentName().containsKey(hardwareName)) updateComponents(endpoint)
         return revisionService.getCurrentRevisionsByComponentName().get(hardwareName)
-    }
-
-    // extracts information about components (Hardware, Service, Instance) from an endpoint-object
-    // and creates new component-objects for them if they are not existing yet.
-    // Additionally it creates relations between these components
-    private fun updateComponents(endpoint: Endpoint): Boolean {
-        val serviceName: String = endpoint.serviceName.toUpperCase()
-        val hardwareName = getHardwareName(endpoint)
-        val instanceName = getInstanceName(endpoint)
-        if (instanceName == "unknown") return false
-        if (!componentRevisionMap!!.containsKey(serviceName)) {
-            val service: Service = serviceService.findByName(serviceName)
-            if (service == null) {
-                serviceService.createService(serviceName)
-            } else revisionService.createRevision(service)
-        }
-        if (!componentRevisionMap!!.containsKey(hardwareName)) {
-            val hardware: Hardware = hardwareService.findByName(hardwareName)
-            if (hardware == null) hardwareService.createHardware(hardwareName) else revisionService.createRevision(hardware)
-        }
-        if (!componentRevisionMap!!.containsKey(instanceName)) {
-            var instance: Instance = instanceService.findByName(instanceName)
-            if (instance == null) {
-                instance = instanceService.createInstance(instanceName)
-                instance.setAnnotation("ad.port", endpoint.port.toString())
-                instance.setAnnotation("ad.ip", getHardwareName(endpoint))
-                instanceService.saveInstance(instance)
-            } else revisionService.createRevision(instance)
-            val instanceRevision: Revision? = componentRevisionMap!![instanceName]
-            val serviceRevision: Revision? = componentRevisionMap!![serviceName]
-            val hardwareRevision: Revision? = componentRevisionMap!![hardwareName]
-            relationService.setInstanceRevisionRelations(instanceRevision, serviceRevision, hardwareRevision)
-        }
-        return true
     }
 
     private fun getInstanceName(endpoint: Endpoint): String {
